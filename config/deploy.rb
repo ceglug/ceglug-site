b29648902cf46467ccddc1c7af9090ec2f36e3d0
root <root@kjhomeappliance.in>
2014-08-17 12:23:13 -0400

    Random

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+set :application, "ceglug.github.io" 
+set :repository,  "/home/chairman/ceglug.github.io"
+
+set :scm, :subversion
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web,"Nginx"                          # Your HTTP server, Apache/etc
+role :app, "Nginx"                      # This may be the same as your `Web` server
+role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+set :user, "chairman"
+
+set :deploy_to, "/home/#{user}/ceglug.github.io"
+
+role :web, "107.170.221.147"
+
+before 'deploy:update', 'deploy:update_jeykill'
+
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    # clear existing _site
+    # build site using jekyll
+    # remove Capistrano stuff from build
+    %x(rm -rf _site/* && jekyll build && rm _site/Capfile && rm -rf _site/config)
+  end
